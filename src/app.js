f041c1dc28f6bf731a32e8c918a14fb1ab6b2208
M-Sayer <haji.san@icloud.com>
2020-07-27 19:49:04 -0400

    created recipes router

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config')
+const recipesRouter = require('./recipes/recipes-router');
 
 const app = express();
 
@@ -14,6 +15,7 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+app.use('/recipes', recipesRouter)
 
 app.get('/', (req, res) => {
   res
