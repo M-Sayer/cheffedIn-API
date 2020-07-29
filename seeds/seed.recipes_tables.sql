80604d613aec5ee0777401e2cabb13ae1c472378
M-Sayer <haji.san@icloud.com>
2020-07-28 21:08:59 -0400

    updated seed files to set seq vals

--- seeds/seed.recipes_tables.sql
+++ seeds/seed.recipes_tables.sql
@@ -15,11 +15,15 @@ INSERT INTO users (id, user_name, full_name, email, password)
   (2, 'user2', 'user two', 'user2@test.com', '$2b$10$9/coW7549wUeO//ELPhLqO4OjVBf7HKtoo63qgecd.buBDcYv4Uaa'),
   (3, 'user3', 'user three', 'user3@test.com', '$2b$10$3LzP6sEW8xja8WfVK78WOOfI7AhDk3jV1BrPN4UCazh1YzjR3kcNa');
 
+SELECT setval ('users_id_seq', 3);
+
 INSERT INTO recipes (id, author_id, title, image, about, prep_time_minutes, prep_time_hours, serving_size, vegetarian, ingredients, steps, date_added)
   VALUES
   (1, 1, 'meat lovers pizza', 'https://loremflickr.com/750/300/landscape?random', 'lorem', '15', '1', '4', 'false', 'some ingredients', 'some steps',  '2020-07-27T18:03:13.574Z'),
   (2, 2, 'soup', 'https://loremflickr.com/750/300/landscape?random', 'lorem', '45', '0', '6', 'true', 'some ingredients', 'some steps',  '2020-07-27T18:03:13.574Z'),
   (3, 3, 'salad', 'https://loremflickr.com/750/300/landscape?random', 'lorem', '15', '0', '4', 'true', 'some ingredients', 'some steps', '2020-07-27T18:03:13.574Z');
+
+SELECT setval ('recipes_id_seq', 3);
 
 INSERT INTO comments (id, recipe_id, author_id, message, date_added)
   VALUES
@@ -27,11 +31,15 @@ INSERT INTO comments (id, recipe_id, author_id, message, date_added)
   (2, 2, 1, 'great', '2020-07-27T18:03:13.574Z'),
   (3, 3, 2, 'lovely', '2020-07-27T18:03:13.574Z');
 
+SELECT setval ('comments_id_seq', 3);  
+
 INSERT INTO lists (id, author_id, list_name)
   VALUES
   (1, 1, 'favorites'),
   (2, 2, 'things i like'),
   (3, 3, 'make later');
+
+SELECT setval ('lists_id_seq', 3);  
 
 INSERT INTO recipes_in_lists (id, recipe_id, list_id)
   VALUES
