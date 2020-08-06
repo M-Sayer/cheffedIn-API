e27caf968c88dda8d1a3b868d6fc89649ff7bb07
M-Sayer <haji.san@icloud.com>
2020-08-05 22:27:28 -0400

    heroku db prep

--- src/server.js
+++ src/server.js
@@ -1,16 +1,16 @@
 const knex = require('knex');
 
 const app = require('./app');
-const { PORT, DB_URL } = require('./config');
+const { PORT, DATABASE_URL } = require('./config');
 
 const db = knex({
   client: 'pg',
-  connection: DB_URL,
+  connection: DATABASE_URL,
 });
 
 app.set('db', db);
 
 
 app.listen(PORT, () => {
-  console.log(`server listening at ${DB_URL} on ${PORT}`);
+  console.log(`server listening at ${DATABASE_URL} on ${PORT}`);
 });
